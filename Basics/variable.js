468e5255209ba5f3f2695b35cbae1a2bde3af906
Rohit  Chhabiraj Yadav <cmrohityadav23@gmail.com>
2023-01-09 08:30:02 +0000

    variable tut done

---
+++ Basics/variable.js
@@ -0,0 +1,10 @@
+const userId="012"
+let userName="cmrohityadav";
+var name="Rohit"
+surName="Yadav"
+
+console.log(userId)
+console.table([userId,userName,name,surName])
+//not allow
+// userId="111";
+// console.log(userId)
