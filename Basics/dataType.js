846ac359ebc57a0130e35597ab12282a64652a9c
Rohit  Chhabiraj Yadav <cmrohityadav23@gmail.com>
2023-01-12 18:57:51 +0000

    Data type

---
+++ Basics/dataType.js
@@ -0,0 +1,22 @@
+"use strict"; //browser ko new version of js batana
+//alert(3+3) //yeh sirf browswer me support krta hai
+console.log(" here im printing Rohit")
+
+//code readibilty
+
+let name="Rohit"
+console.log(typeof name)
+let age=20
+let isloggedIn=false;
+// BigInt
+// String
+//boolean
+//null=> standalone value(represnation of empty value)
+//undefined
+// Symbol => unique 
+
+
+
+//Object
+console.log(typeof undefined)
+console.log(typeof null)
