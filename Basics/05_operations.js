24a25ef103a0055915e12c3ca13b8c2bcd1cdb50
Rohit  Chhabiraj Yadav <cmrohityadav23@gmail.com>
2023-01-17 18:56:30 +0000

    operations

---
+++ Basics/05_operations.js
@@ -0,0 +1,35 @@
+let value=3;
+let negValue=-value;
+// console.log(negValue);
+
+// Basic maths operations
+console.log(1+2)
+console.log(1-2)
+console.log(1*2)
+console.log(1/2)
+console.log(1%2)
+console.log(5**2) // use for the power 
+
+
+let str1="Rohit"
+let str2=" Yadav"
+let str3=str1+str2;
+console.log(str3);
+
+
+console.log("3"+5)
+console.log(6+"3")
+console.log(6+6+"3")
+console.log("3"+6+6)
+
+console.log(true)
+console.log(+true)
+// console.log(true+) error
+
+
+
+
+
+
+
+
